24af2a739134e2f1defc434fd29b6231ea194b06
Rinaldo <mundial.cel11@gmail.com>
2022-07-02 15:53:41 -0300

    rota criada , visao criada e controle article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show" # :id is a placeholder for the id of the article
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
