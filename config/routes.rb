5a5bc8f75392656930158672c89b21154b6a2c6d
Rinaldo <mundial.cel11@gmail.com>
2022-07-04 09:20:57 -0300

    routas , new e controller criada

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show" # :id is a placeholder for the id of the article
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show" # :id is a placeholder for the id of the article
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
