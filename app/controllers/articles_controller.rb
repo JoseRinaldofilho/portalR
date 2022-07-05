22bee5bc764fdef92d68b48f47d8a52ee4d67f9b
Rinaldo <mundial.cel11@gmail.com>
2022-07-05 14:56:50 -0300

    post criado

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,17 @@
 class ArticlesController < ApplicationController
-
+  # busca os artigos e renderiza a view index
   def index
     @articles = Article.all
   end
-
+  # busca o artigo com o id passado e renderiza a view show
   def show
     @article = Article.find(params[:id])
   end
-
+  # renderiza a view new
   def new
     @article = Article.new
   end
-
+  # renderiza a view edit create 
   def create
     @article = Article.new(title: "...", body: "...")
 
@@ -21,6 +21,10 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
      end
   end
+  # renderiza a view edit update
+  def edit
+    @article = Article.find(params[:id])
+  end 
 
 end
 
